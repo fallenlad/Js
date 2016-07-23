8e7fd4dc5bb7777fa062752faebc9d37d54a7254
Dinesh <dineshkumarm@msn.com>
2016-07-24 00:27:06 +0530

    Included comments

--- Ratio.js
+++ Ratio.js
@@ -1,16 +1,18 @@
 //var input = { boys: 1071, girls: 462 };
+//Command line input
+//Sample input - 1071 462
 process.stdin.resume();
 process.stdin.setEncoding("utf-8");
 process.stdin.on("data", function (input) {
     Init(input.replace(/\r\n|\n|\r/gm, '').split(' '));
 });
 
-
+//Find the ratio of boys and girls in a classroom
 function Init(input) {
     var gcd, ratio, classroom;
     if (input.length == 2)
         classroom = { boys: input[0], girls: input[1] };
-    //Get reduced ratio output    
+    //Get simplified ratio output    
     gcd = GetGcd(classroom.boys, classroom.girls);
     ratio = (classroom.boys / gcd) + ':' + (classroom.girls / gcd);
     console.log(ratio);
