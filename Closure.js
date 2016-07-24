4bfb7f2c267a0a99e38758459b230725117af548
Dinesh <dineshkumarm@msn.com>
2016-07-24 23:42:40 +0530

    Added more closure examples

--- Closure.js
+++ Closure.js
@@ -1,3 +1,4 @@
+//Closure example
 var closureLogger = function () {
     var x = 0;
 
@@ -13,3 +14,20 @@ var funcStore2 = closureLogger();
 funcStore();
 funcStore();
 funcStore2();
+
+//another example for closure
+function counter() {
+    var n = 0;
+    return {
+        count: function () { return ++n; },
+        reset: function () { n = 0; }
+    };
+}
+
+var myCounter = counter();
+console.log('Count is ' + myCounter.count());
+console.log('Increment Count to ' + myCounter.count());
+console.log('Increment Count to ' + myCounter.count());
+console.log('Reset counter');
+myCounter.reset();
+console.log('Count is ' + myCounter.count());
